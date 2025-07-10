c2c799da751a6ac9a8b974456a4811999d98fa7f
Isre <alexandrgilev91@gmail.com>
2025-07-10 12:54:09 +0600

    Third commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
    public static void main(String[] args) {
-       System.out.println("Hello Git");
+       System.out.println("Hello Git two");
        System.out.println("Измение с другой вeтки");
+       System.out.println("Change main");
+       String st = "Change main";
    }
 }
