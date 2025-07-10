240d7ec7c2e5b76136f15ae1644f5ea3b9ea4c3b
Isre <alexandrgilev91@gmail.com>
2025-07-10 13:51:44 +0600

    fourth commit change class Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,7 @@ public class Main {
 
    public static void main(String[] args) {
        System.out.println("Hello Git two");
-       System.out.println("Измение с другой вeтки");
-       System.out.println("Change main");
        String st = "Change main";
+       System.out.println(st);
    }
 }
